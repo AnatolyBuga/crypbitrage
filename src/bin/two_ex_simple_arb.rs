9e45cd8858183570dd72228279e74b024e9187bd
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2025-02-16 10:51:49 +0000

    code cleanup

--- src/bin/two_ex_simple_arb.rs
+++ src/bin/two_ex_simple_arb.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeMap},
+    collections::BTreeMap,
     sync::{Arc, Mutex},
 };
 
@@ -7,13 +7,12 @@ use clap::Parser;
 use futures_util::{SinkExt, StreamExt};
 use ordered_float::OrderedFloat;
 use serde::{de::DeserializeOwned, Deserialize, Deserializer, Serialize};
-use serde_json::{json, Value};
+use serde_json::json;
 use tokio::{
-    net::TcpStream,
     sync::mpsc::{UnboundedReceiver, UnboundedSender},
     task::JoinHandle,
 };
-use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 /// Helps parsing cli args.
 /// Part of the binary, since it has nothing to do with the library
@@ -130,118 +129,20 @@ impl From<DeribitData> for CrossExchangeLOB {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(untagged)]
 enum ExchangeData {
-    OKX(OkxData),
+    Okx(OkxData),
     Deribit(DeribitData),
 }
 
 impl From<ExchangeData> for CrossExchangeLOB {
     fn from(ex_data: ExchangeData) -> Self {
         match ex_data {
-            ExchangeData::OKX(data) => data.into(),
+            ExchangeData::Okx(data) => data.into(),
             ExchangeData::Deribit(data) => data.into(),
         }
     }
 }
 
-type Price = OrderedFloat<f64>;
-type Quantity = f64;
-type ExchangeId = u8;
-type _IsBid = bool; // if not bid, then Ask
-/// Bids or Asks
-type LobLeg = BTreeMap<(Price, ExchangeId), Quantity>;
 
-/// Represents any Market Order
-// struct MarketOrder(Price, Quantity, IsBid, ExchangeId);
-
-#[derive(Default, Debug)]
-struct CrossExchangeLOB {
-    pub asks: LobLeg,
-    pub bids: LobLeg,
-}
-
-impl CrossExchangeLOB {
-    pub fn update(&mut self, other: CrossExchangeLOB) {
-        self.asks.extend(other.asks);
-        self.bids.extend(other.bids);
-    }
-}
-
-async fn run_simple_arbitrage(mut receiver: UnboundedReceiver<ExchangeData>) {
-    let _lob = Arc::new(Mutex::new(CrossExchangeLOB::default()));
-
-    while let Some(msg) = receiver.recv().await {
-        let lob = Arc::clone(&_lob);
-        // Assume arbitrage check is a heavy CPU task
-        tokio::task::spawn_blocking(move || {
-            let update_data: CrossExchangeLOB = msg.into();
-            lob.lock().unwrap().update(update_data);
-            println!("{lob:#?}")
-        })
-        .await
-        .expect("Panic on arbitrage calc")
-    }
-}
-fn create_channel<T: Into<CrossExchangeLOB>>() -> (
-    tokio::sync::mpsc::UnboundedSender<T>,
-    tokio::sync::mpsc::UnboundedReceiver<T>,
-) {
-    tokio::sync::mpsc::unbounded_channel() // Returns (Sender<T>, Receiver<T>)
-}
-
-/// sender - sends exchange data to downstream consumers (Arbitrage Strategies)
-///
-async fn exchange_ws_connection<T>(
-    sender: UnboundedSender<T>,
-    exchange_name: String,
-    subscribe_msg: String,
-    url: String,
-) -> JoinHandle<()>
-where
-    T: Into<CrossExchangeLOB> + Send + DeserializeOwned + 'static,
-{
-    let (ws_stream, _) = connect_async(url)
-        .await
-        .expect("WebSocket connection failed");
-    let (mut write, mut ws_reader) = ws_stream.split();
-
-    // Send subscription request
-    write
-        .send(Message::Text(subscribe_msg.into()))
-        .await
-        .unwrap_or_else(|e| {
-            panic!(
-                "{} Failed to send subscription message: {}",
-                exchange_name, e
-            )
-        });
-
-    println!("Subscribed to {}", exchange_name);
-
-    tokio::spawn(async move {
-        while let Some(msg) = ws_reader.next().await {
-            match msg {
-                Ok(Message::Text(text)) => {
-                    println!("{}: {}", exchange_name, text);
-                    // TODO Optimisation decode only data , not the whole message
-                    match serde_json::from_slice::<T>(text.as_bytes()) {
-                        Ok(exchange_data) => sender.send(exchange_data).unwrap_or_else(|e| {
-                            panic!(
-                                "{} data deserialized, but couldn't send: {}",
-                                exchange_name, e
-                            )
-                        }),
-                        Err(e) => println!("{}: couldn't deserialize: {e}", exchange_name),
-                    }
-                }
-                Ok(_) => (),
-                Err(e) => {
-                    eprintln!("{} WebSocket error: {:?}", exchange_name, e);
-                    break;
-                }
-            }
-        }
-    })
-}
 
 #[tokio::main]
 async fn main() {
@@ -288,7 +189,7 @@ async fn main() {
     let task3 = tokio::spawn(run_simple_arbitrage(receiver));
 
     // https://stackoverflow.com/questions/69638710/when-should-you-use-tokiojoin-over-tokiospawn
-    tokio::join!(task1, task2, task3);
+    let _ = tokio::join!(task1, task2, task3);
 
     println!("WebSocket connection closed.");
 }
